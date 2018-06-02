6d89886f247d9ef84b532598fc6b8951c79ee972
Jonathan Goldfarb <jgoldfar@gmail.com>
2018-06-02 16:14:26 -0400

    Add a couple more test cases (in particular, test for more general convergence fact for defined norms; this is the key requirement.)

--- test/norms.jl
+++ test/norms.jl
@@ -1,3 +1,15 @@
+@static if VERSION < v"0.7-"
+    using Base.Test
+    using Compat: range
+else
+    using Test
+end
+
+using WeightedNorms
+if !isdefined(:WN)
+    const WN = WeightedNorms
+end
+
 const testGridLength = 100
 const tolerance = 1e-10
 testgridArchetype = range(0, stop=1, length=testGridLength)
@@ -68,7 +80,92 @@ testGrids = [testgridArchetype, collect(testgridArchetype)]
         v2 = @inferred func(testZerosMat, testgrid, testgrid)
         @test isapprox(v2, 0.0)
     end
+
 end
+
+@testset "Convergence for Grid Input" begin
+    ConvergenceNRange = [2^n for n in 3:6]
+    L1ErrorValues = zeros(length(ConvergenceNRange))
+    L2ErrorValues = zeros(length(ConvergenceNRange))
+
+    @testset "Constant Function C=$C (so norm is domain size), LX=$LX, LT=$LT" for C in -1:1, LX in 3:5, LT in 1:4
+        for (i,N) in enumerate(ConvergenceNRange)
+            xTestGrid = collect(range(-1, stop=(-1+LX), length=N+1))
+            tTestGrid = collect(range(0, stop=(0+LT), length=N))
+            testfunc = C*ones(N+1, N)
+
+            testfuncL1NormExact = abs(C)*LX*LT
+            L1ErrorValues[i] = abs(testfuncL1NormExact - WN.norm_l1(testfunc, xTestGrid, tTestGrid))
+
+            testfuncL2NormExact = sqrt(abs(C)*LX*LT)
+            L2ErrorValues[i] = abs(testfuncL2NormExact - WN.norm_l2(testfunc, xTestGrid, tTestGrid))
+        end
+
+        @test all(e <= tolerance for e in L1ErrorValues)
+        @test all(e <= tolerance for e in L2ErrorValues)
+    end
+
+    fill!(L1ErrorValues, 0)
+    fill!(L2ErrorValues, 0)
+
+    # Nonlinear function with changing sign.
+    for (i,N) in enumerate(ConvergenceNRange)
+        xTestGrid = collect(range(-1, stop=0.5, length=N+1))
+        tTestGrid = collect(range(0, stop=1, length=N))
+        testfunc1 = [x * (4*t - 1) for x in xTestGrid, t in tTestGrid]
+
+        testfunc1L1NormExact = 25//32
+        L1ErrorValues[i] = abs(testfunc1L1NormExact - WN.norm_l1(testfunc1, xTestGrid, tTestGrid))
+
+        testfunc1L2NormExact = 7//8
+        L2ErrorValues[i] = abs(testfunc1L2NormExact - WN.norm_l2(testfunc1, xTestGrid, tTestGrid)^2)
+    end
+    @test all(d <=0 for d in diff(L1ErrorValues))
+    @test all(d <=0 for d in diff(L2ErrorValues))
+
+end
+
+@testset "Convergence for Range Input" begin
+    ConvergenceNRange = [2^n for n in 3:6]
+    L1ErrorValues = zeros(length(ConvergenceNRange))
+    L2ErrorValues = zeros(length(ConvergenceNRange))
+
+    @testset "Constant Function C=$C (so norm is domain size), LX=$LX, LT=$LT" for C in -1:1, LX in 3:5, LT in 1:4
+        for (i,N) in enumerate(ConvergenceNRange)
+            xTestGrid = range(-1, stop=(-1+LX), length=N+1)
+            tTestGrid = range(0, stop=(0+LT), length=N)
+            testfunc = C*ones(N+1, N)
+
+            testfuncL1NormExact = abs(C)*LX*LT
+            L1ErrorValues[i] = abs(testfuncL1NormExact - WN.norm_l1(testfunc, xTestGrid, tTestGrid))
+
+            testfuncL2NormExact = sqrt(abs(C)*LX*LT)
+            L2ErrorValues[i] = abs(testfuncL2NormExact - WN.norm_l2(testfunc, xTestGrid, tTestGrid))
+        end
+
+        @test all(e <= tolerance for e in L1ErrorValues)
+        @test all(e <= tolerance for e in L2ErrorValues)
+    end
+
+    fill!(L1ErrorValues, 0)
+    fill!(L2ErrorValues, 0)
+
+    # Nonlinear function with changing sign.
+    for (i,N) in enumerate(ConvergenceNRange)
+        xTestGrid = range(-1, stop=0.5, length=N+1)
+        tTestGrid = range(0, stop=1, length=N)
+        testfunc1 = [x * (4*t - 1) for x in xTestGrid, t in tTestGrid]
+
+        testfunc1L1NormExact = 25//32
+        L1ErrorValues[i] = abs(testfunc1L1NormExact - WN.norm_l1(testfunc1, xTestGrid, tTestGrid))
+
+        testfunc1L2NormExact = 7//8
+        L2ErrorValues[i] = abs(testfunc1L2NormExact - WN.norm_l2(testfunc1, xTestGrid, tTestGrid)^2)
+    end
+    @test all(d <=0 for d in diff(L1ErrorValues))
+    @test all(d <=0 for d in diff(L2ErrorValues))
+end
+
 
 @testset "Edge case regression tests" begin
     twoValGrid = [0.0, 1.0]
